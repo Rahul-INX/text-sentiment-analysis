73fbb8458373f14861b6bfa6a1a844284157b00f
Rahul Harivansh Fatyal <76155562+Rahul-INX@users.noreply.github.com>
2023-06-13 08:06:14 +0530

    everything done except depression detection

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request, redirect
-import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 import numpy as np
-import datetime
-
-# Set the minimum number of entries required for depression calculation
-MIN_ENTRIES = 10
-
+import tensorflow as tf
+import json
+import os
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+import random
 
 app = Flask(__name__)
-app.config['THRESHOLD'] = 50
+app.secret_key = 'your_secret_key'  # Set a secret key for session management
+
+# Configure session storage
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 # Load the tokenizer
 with open('tokenizer.pkl', 'rb') as f:
@@ -18,9 +22,6 @@ with open('tokenizer.pkl', 'rb') as f:
 
 # Load the emotion trained model
 model = tf.keras.models.load_model('emotion_model_trained.h5')
-
-# Create an empty list to store the input and result history
-history = []
 
 def preprocess_text(text):
     # Tokenize the text
@@ -38,113 +39,159 @@ def predict_emotion(text):
     # Predict the emotion probabilities
     predicted_probs = model.predict(processed_text)[0]
 
-    # Get the index of the predicted emotion
-    predicted_index = np.argmax(predicted_probs)
+    # Get the indices of the top 3 probabilities in descending order
+    top_indices = np.argsort(predicted_probs)[::-1][:3]
 
-    # Map the predicted index to the corresponding emotion
+    # Map the indices to the corresponding emotions and probabilities
     emotions = ['anger', 'fear', 'sadness', 'surprise', 'joy', 'love']
-    predicted_emotion = emotions[predicted_index]
+    top_emotions = [emotions[idx] for idx in top_indices]
+    top_probabilities = [float(predicted_probs[idx]) for idx in top_indices]  # Convert to float
 
-    return predicted_emotion
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Get the current timestamp
 
-def save_history(text, emotion):
-    # Get the current timestamp
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return top_emotions, top_probabilities, timestamp  # Return emotions, probabilities, and timestamp
 
-    # Append the input, emotion, and timestamp to the history list
-    history.insert(0, {'text': text, 'emotion': emotion, 'timestamp': timestamp})
-
-    # Save the updated history to a file
-    with open('history.txt', 'a') as f:
-        f.write(f"{text}\t{emotion}\t{timestamp}\n")
-
-def analyze_history(window_size):
-    current_time = datetime.datetime.now()
-    start_time = current_time - window_size
-
-    # Filter history within the window size
-    filtered_history = [entry for entry in history if start_time <= datetime.datetime.strptime(entry['timestamp'], '%Y-%m-%d %H:%M:%S') <= current_time]
-
-    # Calculate the emotion frequencies
-    emotion_counts = {}
-    for entry in filtered_history:
-        emotion = entry['emotion']
-        emotion_counts[emotion] = emotion_counts.get(emotion, 0) + 1
-
-    # Calculate the average emotion
-    total_entries = len(filtered_history)
-    average_emotion = {emotion: count / total_entries * 100 for emotion, count in emotion_counts.items()}
-
-    return average_emotion
-
-def check_depression_warning(min_entries):
-    if len(history) < min_entries:
-        return False
-
-    # Calculate average emotions for different window sizes
-    one_hour_average = analyze_history(datetime.timedelta(hours=1))
-    one_day_average = analyze_history(datetime.timedelta(days=1))
-    one_month_average = analyze_history(datetime.timedelta(days=30))
-
-    # Check if specific emotions exceed the threshold
-    threshold = app.config['THRESHOLD']  # Get threshold from app configuration
-    threshold_emotions = ['anger', 'sadness', 'fear', 'surprise']
-    depression_warning = False
-
-    for emotion in threshold_emotions:
-        if (
-            one_hour_average.get(emotion, 0) > threshold or
-            one_day_average.get(emotion, 0) > threshold or
-            one_month_average.get(emotion, 0) > threshold
-        ):
-            depression_warning = True
-            break
-
-    return depression_warning
+def save_history(history):
+    with open('history.json', 'w') as f:
+        json.dump(history, f)
 
 def load_history():
-    try:
-        with open('history.txt', 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                entry = line.strip().split('\t')
-                history.append({'text': entry[0], 'emotion': entry[1], 'timestamp': entry[2]})
-    except FileNotFoundError:
-        # Create the history file if it doesn't exist
-        open('history.txt', 'w').close()
+    if not os.path.exists('history.json'):
+        with open('history.json', 'w') as f:
+            f.write("[]")  # Write an empty list to the file
+        return []  # Return an empty list as the history
 
+    try:
+        with open('history.json', 'r') as f:
+            history = json.load(f)
+    except:
+        history = []
+    return history
+
+def predict_inferred_emotions(emotions, probabilities, threshold):
+    inferred_emotions = []
+
+    # Define the rules for inferring emotions based on the given emotions
+    rules = [
+    (('joy', 'love'), 'bliss'),
+    (('surprise', 'love'), 'affection'),
+    (('surprise', 'joy'), 'elation'),
+    (('sadness', 'joy'), 'nostalgia'),
+    (('sadness', 'love'), 'melancholy'),
+    (('fear', 'joy'), 'excitement'),
+    (('fear', 'love'), 'longing'),
+    (('anger', 'joy'), 'exasperation'),
+    (('anger', 'love'), 'passion'),
+    (('sadness', 'surprise', 'love'), 'bittersweet'),
+    (('sadness', 'surprise'), 'disappointment'),
+    (('anger', 'surprise'), 'outrage'),
+    (('fear', 'surprise'), 'anxiety'),
+    (('surprise',), 'surprise'),
+    (('anger', 'sadness'), 'resentment'),
+    (('anger', 'fear', 'sadness'), 'resigned'),
+    (('anger', 'fear'), 'frustration'),
+    (('sadness', 'fear'), 'despair'),
+    (('fear',), 'fear'),
+    (('sadness',), 'sadness'),
+    (('anger',), 'anger'),
+    (('joy',), 'joy'),
+    (('love',), 'love'),
+    (('joy', 'surprise', 'love'), 'delight'),
+    (('anger', 'surprise', 'joy'), 'indignation'),
+    (('fear', 'sadness', 'joy'), 'admiration'),
+    (('fear', 'sadness', 'love'), 'sorrow'),
+    (('anger', 'fear', 'joy'), 'outrage'),
+    (('anger', 'fear', 'love'), 'rage'),
+    (('surprise', 'sadness', 'fear'), 'awe'),
+    (('surprise', 'sadness', 'love'), 'amazement'),
+    (('surprise', 'sadness', 'joy'), 'amusement'),
+    (('fear', 'surprise', 'love'), 'trepidation'),
+    (('anger', 'sadness', 'love'), 'heartache'),
+    (('anger', 'surprise', 'sadness'), 'fury'),
+    (('anger', 'surprise', 'fear'), 'hostility'),
+    (('sadness', 'fear', 'love'), 'grief'),
+    (('sadness', 'surprise', 'joy'), 'regret'),
+    (('sadness', 'surprise', 'fear'), 'pity'),
+    (('sadness', 'joy', 'love'), 'yearning'),
+    (('anger', 'joy', 'love'), 'zeal')
+    ]
+
+
+    filtered_emotions = []
+
+    # Filter emotions based on probability threshold
+    for i in range(len(emotions)):
+        if probabilities[i] >= threshold:
+            filtered_emotions.append(emotions[i])
+
+    # Check if any of the defined rules match the filtered emotions
+    for rule_emotions, inferred_emotion in rules:
+        if all(emotion in filtered_emotions for emotion in rule_emotions):
+            inferred_emotions.append(inferred_emotion)
+
+    # Remove atomic emotions from inferred emotions if its size is greater than 3
+    if len(inferred_emotions) > 3:
+        inferred_emotions = [emotion for emotion in inferred_emotions if emotion not in ('anger', 'fear', 'sadness', 'surprise', 'joy', 'love')]
+
+    print("THRESHOLD:", threshold)
+    print("FILTERED EMOTION:", filtered_emotions)
+    print("INFERRED EMOTION:", inferred_emotions)
+    return inferred_emotions
+
+def get_random_string():
+    with open('test_text.txt', 'r') as file:
+        lines = file.readlines()
+        random_string = random.choice(lines).strip()
+        return random_string
+
+
+history = load_history()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    inferred_emotions = []
+    
+    if 'threshold' in session:
+        threshold = session['threshold']  # Get the threshold value from the session
+
+    random_string = session.pop('random_string', '')  # Get and remove the random_string value from the session or assign an empty string
+
     if request.method == 'POST':
         text = request.form['text']
-        emotion = predict_emotion(text)
-        save_history(text, emotion)
+        emotions, probabilities, timestamp = predict_emotion(text)
+        inferred_emotions = predict_inferred_emotions(emotions, probabilities, threshold)
+        entry = {
+            'text': text,
+            'emotions': emotions,
+            'probabilities': probabilities,
+            'inferred_emotions': inferred_emotions,
+            'timestamp': timestamp
+        }
+        history.insert(0, entry)
+        save_history(history)
         return redirect('/')
-    else:
-        emotion = None
-        threshold = app.config['THRESHOLD']
-        depression_warning = check_depression_warning(MIN_ENTRIES)
-        return render_template('index.html', emotion=emotion, history=history, depression_warning=depression_warning, threshold=threshold)
+    
+    return render_template('index.html', history=history, inferred_emotions=inferred_emotions, threshold=threshold, random_string=random_string)
+
 
 
 @app.route('/clear', methods=['POST'])
 def clear_history():
     history.clear()
-    with open('history.txt', 'w') as f:
-        pass  # Clear the history file
+    save_history(history)
     return redirect('/')
-
-
 
 @app.route('/set_threshold', methods=['POST'])
 def set_threshold():
-    threshold = int(request.form['threshold'])
-    app.config['THRESHOLD'] = threshold
+    threshold = float(request.form['threshold'])
+    session['threshold'] = threshold  # Update the threshold value in the session
+    return redirect('/')
+@app.route('/random', methods=['POST'])
+def random_text():
+    random_string = get_random_string()
+    session['random_string'] = random_string
     return redirect('/')
 
 
 if __name__ == '__main__':
-    load_history()
     app.run(debug=True)
-
